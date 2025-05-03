ab92957628e886498f7007e840a5f1c777925997
Brian C. Ferrari <48806333+Cavenfish@users.noreply.github.com>
2025-05-03 13:57:27 +0200

    remove whitespace

--- src/chars/utils.rs
+++ src/chars/utils.rs
@@ -11,25 +11,15 @@ pub fn get_char(id: u32) -> Result<CharInfo, Error> {
     let row: CharInfo = db.query_row(&query, [], |row| {
         Ok(CharInfo {
             id: row.get(0)?,
-
             name: row.get(1)?,
-
             vocation: row.get(2)?,
-
             level: row.get(3)?,
-
             ml: row.get(4)?,
-
             fl: row.get(5)?,
-
             sl: row.get(6)?,
-
             al: row.get(7)?,
-
             cl: row.get(8)?,
-
             dl: row.get(9)?,
-
             shl: row.get(10)?,
         })
     })?;
@@ -45,25 +35,15 @@ pub fn get_all_chars() -> Result<Vec<CharInfo>, Error> {
     let tmp = stmt.query_map([], |row| {
         Ok(CharInfo {
             id: row.get(0)?,
-
             name: row.get(1)?,
-
             vocation: row.get(2)?,
-
             level: row.get(3)?,
-
             ml: row.get(4)?,
-
             fl: row.get(5)?,
-
             sl: row.get(6)?,
-
             al: row.get(7)?,
-
             cl: row.get(8)?,
-
             dl: row.get(9)?,
-
             shl: row.get(10)?,
         })
     })?;
